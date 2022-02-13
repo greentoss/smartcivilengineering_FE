9344857716a14e6f0216af8d8482c3e4153ec27a
greentoss <mrsoberman1@gmail.com>
2022-02-13 23:30:31 +0200

    fixed routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import {Route, Router, Routes} from "react-router-dom";
+import {BrowserRouter as Router,  Routes,  Route} from "react-router-dom";
 import About from "./components/About";
 import Main from "./components/Main";
 import Services from "./components/Services";
@@ -9,25 +9,19 @@ import ClientError from "./components/ClientError";
 import HeaderMain from "./components/HeaderMain";
 
 function App() {
-  return (<>
-    <Router >
-        <HeaderMain />
-        <Routes>
-            <Route exact path='/' element = {<Main />} />
-            <Route path='/about' element = {<About />} />
-            <Route path='/articles' element = {<Articles />} />
-            <Route path='/services' element = {<Services />} />
-            <Route path = '*' element = {<ClientError />} />
-        </Routes>
+  return (
+    <>
+        <Router >
+            <HeaderMain />
+            <Routes>
+                <Route exact path='/' element = {<Main />} />
+                <Route path='/about' element = {<About />} />
+                <Route path='/articles' element = {<Articles />} />
+                <Route path='/services' element = {<Services />} />
+                <Route path = '*' element = {<ClientError />} />
+            </Routes>
 
-
-        {/*<div className="App">*/}
-        {/*    <header className="App-header">*/}
-        {/*        <h2>SMART CIVIL ENGINEERING</h2>*/}
-        {/*        <img src={logo} className="App-logo" alt="logo" />*/}
-        {/*    </header>*/}
-        {/*</div>*/}
-    </Router>
+        </Router>
     </>
   );
 }
--- src/components/ClientError/index.js
+++ src/components/ClientError/index.js
@@ -3,7 +3,7 @@ import react from 'react';
 function ClientError() {
     return (<>
 
-            <h2>Error 404 wrong address, try again</h2>
+            <h2>Error 404: wrong address, try again!</h2>
 
         </>
     );
