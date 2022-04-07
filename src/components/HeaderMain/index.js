ecbefaaebcdfdbca6722aa4c607fd9aa1a850805
greentoss <mrsoberman1@gmail.com>
2022-04-07 22:05:49 +0300

    refactor header

--- src/components/HeaderMain/index.js
+++ src/components/HeaderMain/index.js
@@ -1,52 +1,43 @@
 import {Link} from "react-router-dom";
 import logo from "../../logo.svg";
 import headerStyle from './index.module.css';
-import './customNav.css'
+import './customNav.css';
 import LanguageToggler from "../LanguageToggler/LanguageToggler";
 
 function HeaderMain() {
     return (
         <>
-            <div className={headerStyle.header}>
-                <div className="container" >
-                    <div className="row justify-content-between align-items-center pt-3 pb-3" >
-                        <div className='row font-weight-bold col-lg-5 col-sm-4 p-0 pl-3 d-flex align-items-center' >
-                            <h2 className={headerStyle.headingMain}>SMART CIVIL ENGINEERING</h2>
-                            <div className='p-0'><img src={logo} className="App-logo" alt="logo" /></div>
-                        </div>
-
-                        <nav className="navbar navbar-expand-lg navbar-light col-lg-6 col-sm-3 p-0 pl-sm-0 ">
-                            <div className="container-fluid p-0">
-                                {/*<a className="navbar-brand font-weight-bold col-3" href="#">Games</a>*/}
-                                <button className="navbar-toggler bg-dark customToggleBtn" type="button" data-toggle="collapse"
-                                        data-target="#navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                                    <span className="navbar-toggler-icon"/>
-                                </button>
-                                <div className="collapse navbar-collapse d-flex justify-content-center" id="navbarNavAltMarkup">
-                                    <ul className="navbar-nav d-flex justify-content-lg-around">
-                                        <li className='mx-lg-1 mt-sm-2 nav-item  active' ><Link to='/' ><button type='button' className='customNavBtn nav-link btn-lg text-white btn-outline-dark text-sm-center'>Main</button></Link></li>
-                                        <li className='mx-lg-1 mt-sm-2 nav-item '><Link to='/about' className=''><button type='button' className='customNavBtn nav-link btn-lg text-white btn-outline-dark text-sm-center'>About</button></Link></li>
-                                        <li className='mx-lg-1 mt-sm-2 nav-item '><Link to='/articles'><button type='button' className='customNavBtn nav-link btn-lg text-white btn-outline-dark text-sm-center'>Articles</button></Link></li>
-                                        <li className='mx-lg-1 mt-sm-2 nav-item '><Link to='/services'><button type='button' className='customNavBtn nav-link btn-lg text-white btn-outline-dark text-sm-center'>Services</button></Link></li>
-                                        <li className='mx-lg-1 mt-sm-2 nav-item '><Link to='/contacts'><button type='button' className='customNavBtn nav-item nav-link btn-lg text-white btn-outline-dark text-sm-center'>Contacts</button></Link></li>
-                                    </ul>
-                                    {/*<a className="d-block p-2 bg-dark text-white d-sm-none d-md-none d-lg-block" href="#">Push to*/}
-                                    {/*    start</a>*/}
+            <div className='header'>
+                <div className="header-blue">
+                    <nav className="navbar navbar-dark navbar-expand-md navigation-clean-search">
+                        <div className="container p-0">
+                            <h2 className='headingMain col-xl-5 col-lg-3'>SMART CIVIL ENGINEERING</h2>
+                            <div className='logoTemp'><img src={logo} className="App-logo" alt="logo" /></div>
+                            {/*<a className="navbar-brand" href="#">SMART CIVIL ENGINEERING</a>*/}
+                            <button className="navbar-toggler bg-dark" data-toggle="collapse" data-target="#navcol-1">
+                                <span className="sr-only">Toggle navigation</span>
+                                <span className="navbar-toggler-icon"/>
+                            </button>
+                            <div className="collapse navbar-collapse justify-content-between col-xl-7 col-lg-5" id="navcol-1">
+                                <ul className="nav navbar-nav justify-content-center w-100 ">
+                                    <li className='nav-item mx-1  active' ><Link to='/' ><button type='button' className='customNavBtn nav-link btn-lg text-white btn-outline-dark text-center'>Main</button></Link></li>
+                                    <li className='nav-item mx-1 '><Link to='/about' ><button type='button' className='customNavBtn nav-link btn-lg text-white btn-outline-dark text-center'>About</button></Link></li>
+                                    <li className='nav-item mx-1 '><Link to='/articles'><button type='button' className='customNavBtn nav-link btn-lg text-white btn-outline-dark text-center'>Articles</button></Link></li>
+                                    <li className='nav-item mx-1 '><Link to='/services'><button type='button' className='customNavBtn nav-link btn-lg text-white btn-outline-dark text-center'>Services</button></Link></li>
+                                    <li className='nav-item mx-1 '><Link to='/contacts'><button type='button' className='customNavBtn nav-item nav-link btn-lg text-white btn-outline-dark text-center'>Contacts</button></Link></li>
+                                </ul>
+                                <div className='login-menu flex-column col-xl-2 col-lg-2'>
+                                    <div className='languages mb-2 d-flex justify-content-around'>
+                                        <span className='text-info d-inline-block'>en</span>
+                                        <span className='text-secondary d-inline-block'>|</span>
+                                        <span className='text-info d-inline-block'>ua</span>
+                                    </div>
+                                    <button className='btn btn-sm btn-outline-info w-100 mb-2' type='button'>Login</button>
+                                    <button className='btn btn-sm btn-outline-info w-100 mb-2' type='button'>Signup</button>
                                 </div>
-
                             </div>
-                        </nav>
-                        <div className='login-menu flex-column col-lg-1'>
-                            <div className='languages mb-2 d-flex justify-content-around'>
-                                <span className='text-info d-inline-block'>en</span>
-                                <span className='text-secondary d-inline-block'>|</span>
-                                <span className='text-info d-inline-block'>ua</span>
-                            </div>
-                            <button className='btn btn-sm btn-outline-info w-100 mb-2' type='button'>Login</button>
-                            <button className='btn btn-sm btn-outline-info w-100 mb-2' type='button'>Signup</button>
                         </div>
-
-                    </div>
+                    </nav>
                 </div>
             </div>
             <div className='green-strype'/>
--- src/components/MainCarouselHeading/index.js
+++ src/components/MainCarouselHeading/index.js
@@ -5,7 +5,7 @@ function MainCarouselHeading() {
         <>
             <div className="carousel-caption d-md-block d-flex">
                 <h2 className='carousel-h'>We will rebuild our country!</h2>
-                <p className='carousel-p'>consultations about damaged constructions, rebuilding and reconstruction of injured infrastructiure</p>
+                <p className='carousel-p'>we porpouse YOU : <br/> consultations about damaged constructions, rebuilding and reconstruction of injured infrastructiure</p>
             </div>
         </>
     );
