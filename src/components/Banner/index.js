ad8bdeb257cd442db8f3cc21e659a6e66d891446
greentoss <mrsoberman1@gmail.com>
2022-04-07 23:23:44 +0300

    add banner close button

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,11 +1,21 @@
 import './banner.css';
+import {useState} from "react";
 
 function HeaderMain() {
+    const [isActiveBanner, setActiveBanner] = useState('banner-active');
+
+    let handleBannerClose = () => {
+        setActiveBanner('banner-notActive');
+    }
+
     return (
         <>
-            <div className='banner'>
+            <div className={`banner ${isActiveBanner}`}>
                 <div className='banner-box'>
                     <span className='banner-text'>Русскій воєнний корабль, іді нахуй!</span>
+                    <button type="button" className="btn-close btn-close-white banner-btn-close align-self-start" aria-label="Close"
+                        onClick={handleBannerClose}
+                    />
                 </div>
             </div>
         </>
